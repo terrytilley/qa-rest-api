145e5eb0345a591a3ca00561da5e5a3df5f06289
Terry <terry@terrytilley.com>
2016-10-18 21:04:59 +0100

    added body-parser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 
-var express = require('express');
-var app     = express();
-const port  = process.env.PORT || 3000;
+var express     = require('express');
+var app         = express();
+var jsonParser  = require('body-parser').json;
+const port      = process.env.PORT || 3000;
+
+app.use(jsonParser());
 
 app.listen(port, () => {
   console.log(`Express server is listening on port ${port}`);
