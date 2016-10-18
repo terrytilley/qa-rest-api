6ae01f000e9e32836341cad01d5c6548abfe833e
Terry <terry@terrytilley.com>
2016-10-18 22:03:18 +0100

    added error handler

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Express server is listening on port ${port}`);
 });
--- routes.js
+++ routes.js
@@ -60,7 +60,15 @@ router.delete('/:qID/answers/:aID', (req, res) => {
 // POST /questions/:qID/answers/:aID/vote-up
 // POST /questions/:qID/answers/:aID/vote-down
 // Vote on a specific answer
-router.post('/:qID/answers/:aID/vote-:dir', (req, res) => {
+router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
+  if (req.params.dir.search(/^(up|down)$/) === -1) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+  }, (req, res) => {
   res.json({
     response: "You sent me a POST request to /vote-" + req.params.dir,
     questionId: req.params.qID,
